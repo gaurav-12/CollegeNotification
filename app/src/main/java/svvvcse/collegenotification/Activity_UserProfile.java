486a370fc413a227907d4238157ca6eaafbd28f2
Srikanth Maturu <srikanthmaturu92@gmail.com>
2018-09-22 21:14:37 -0500

    Basic App is ready.

--- app/src/main/java/svvvcse/collegenotification/Activity_UserProfile.java
+++ app/src/main/java/svvvcse/collegenotification/Activity_UserProfile.java
@@ -1,13 +1,36 @@
 package svvvcse.collegenotification;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
 
 public class Activity_UserProfile extends AppCompatActivity {
+    Button createNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+        createNote = findViewById(R.id.create_note_button);
+    }
+
+
+    private void registerButtonsClickListener() {
+        createNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Activity_UserProfile.this, CreateNotice.class);
+                startActivity(i);
+            }
+        });
     }
 }
---
+++ app/src/main/java/svvvcse/collegenotification/CreateNotice.java
@@ -0,0 +1,13 @@
+package svvvcse.collegenotification;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateNotice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_notice);
+    }
+}
